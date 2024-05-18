3bd0eab2bc05ce8c9c0615948d6d13b08d6abe4a
Jossy Benitez <jossybr94@gmail.com>
2024-05-18 14:06:23 -0500

    Se crea estructura de proyecto con laravel y se realizan migraciones para la base de datos

---
+++ database/migrations/2024_05_18_162846_create_doc_documento_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doc_documento', function (Blueprint $table) {
+            $table->integer('doc_id')->primary();
+            $table->string('doc_nombre', 50);
+            $table->string('doc_codigo', 20)->unique();
+            $table->string('doc_contenido', 4000);
+            $table->integer('doc_id_tipo')->unsigned(); 
+            $table->integer('doc_id_proceso')->unsigned();
+            $table->timestamps();
+
+            // claves foráneas
+            $table->foreign('doc_id_tipo')->references('tip_id')->on('tip_tipo_doc')->onDelete('cascade');
+            $table->foreign('doc_id_proceso')->references('pro_id')->on('pro_proceso')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doc_documento');
+    }
+};
